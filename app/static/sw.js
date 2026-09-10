0b8077992b40dd0c52f5e127bb365337e21412c3
Vitor de Souza <victordsouzajr99@gmail.com>
2026-09-10 17:16:07 -0300

    Header desktop: logo mais à esquerda e afastada do link Início

--- app/static/sw.js
+++ app/static/sw.js
@@ -5,12 +5,12 @@
    ============================================================ */
 "use strict";
 
-const CACHE_PREFIX = "igrejacaminhar-v12";
+const CACHE_PREFIX = "igrejacaminhar-v13";
 const PRECACHE_URLS = [
   "/",
-  "/static/css/style.css?v=18",
-  "/static/css/responsive.css?v=18",
-  "/static/js/main.js?v=18",
+  "/static/css/style.css?v=19",
+  "/static/css/responsive.css?v=19",
+  "/static/js/main.js?v=19",
   "/static/images/pwa/icon-192.png",
   "/static/images/pwa/icon-512.png",
   "/static/images/pwa/icon-maskable-512.png",
